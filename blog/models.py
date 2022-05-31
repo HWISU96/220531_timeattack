8144388de039cb8967f04efe8706972d4ddbbf3a
HWISU96 <soshyzx123@naver.com>
2022-05-31 10:13:13 +0900

    [feat] 4번

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    class Meta:
+        db_table = "category"
+
+    category_title = models.CharField(max_length=20, null=False)
+    category_explain = models.CharField(max_length=256, null=False)
+
+
+class Article(models.Model):
+    class Meta:
+        db_table = "article"
+
+    article_title = models.CharField(max_length=20, null=False)
+    article_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    article_content = models.CharField(max_length=256, default='')
